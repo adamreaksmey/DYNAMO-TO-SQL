ad52fa66342ec8f1ae90fe6031fa7c64dff3a842
RuenChanPiseth <127710222+RuenChanPiseth@users.noreply.github.com>
2024-05-12 16:34:47 +0700

    add select

--- src/components/homepage/Convertor.tsx
+++ src/components/homepage/Convertor.tsx
@@ -1,6 +1,25 @@
 //
+
+import React from 'react'
+import { Select, SelectItem } from '@nextui-org/react'
+import { dataitem } from './data'
+
 const Convertor = () => {
-  return <div></div>
+  return (
+    <div>
+      <div>
+        <div>
+          <Select size={'md'} label="Select an animal" className="max-w-xs">
+            {dataitem.map((animal) => (
+              <SelectItem key={animal.value} value={animal.value}>
+                {animal.label}
+              </SelectItem>
+            ))}
+          </Select>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Convertor
